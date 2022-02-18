b630d16a81c68e28186afbb8b31dfb0d4f3f0a1f
Error <error>
2022-02-18 14:08:16 +0400

    main by error

--- day_02/Objects 00/main.js
+++ day_02/Objects 00/main.js
@@ -4,3 +4,25 @@ var chien = {};
 console.log(chien);
 // Ajoutez le nom, les pattes, la couleur, l'âge et les propriétés d'écorce pour l'objet chien.
 // La propriété bark est une méthode qui renvoie woof woof
+let dog_3 = {
+  name: "medor",
+  pattes: "panzani",
+  color: "chair",
+  age: "detre adulte",
+  bark: bark(),
+};
+function bark() {
+  let text = "Woof Woof";
+  return text;
+}
+console.log("1.3", dog_3);
+console.log(dog_3.name);
+console.log(dog_3.pattes);
+console.log(dog_3.bark);
+
+dog_3.race = getDogInfo();
+function getDogInfo(params) {
+  let text = "malinois";
+  return text;
+}
+console.log(dog_3);
